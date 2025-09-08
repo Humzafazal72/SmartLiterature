84507d5f0b2d3477660ac84383115ecca82abe7d
humzafazal72 <abbasisaud99@gmail.com>
2025-09-08 15:14:07 +0500

    updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from app.routers import auth, agent_api
+from app.routers import auth, agent_api, main
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(agent_api.router, prefix="/agent", tags=["agent"])
+app.include_router(main.router, prefix="", tags=["main"])
 
 @app.get("/")
 def main():
